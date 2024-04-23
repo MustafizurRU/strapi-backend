6ead08c6743dcfff93eb28a62feba6c8a269dfa1
MustafizurRU <mmrsaju.cd@gmail.com>
2024-04-23 13:52:31 +0900

    first commit

---
+++ src/api/user-pose-image/routes/user-pose-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-pose-image router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-pose-image.user-pose-image');
---
+++ src/api/user-pose-image/services/user-pose-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-pose-image service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::user-pose-image.user-pose-image');
